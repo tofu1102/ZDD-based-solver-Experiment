17ef7a6ce8e23e35807a499f32f7d6f0a15791e4
yuta <p3mf5cxkh2itof@gmail.com>
2024-01-30 16:36:28 +0900

    analyzed

--- analyze_all_result.sh
+++ analyze_all_result.sh
@@ -3,5 +3,5 @@
 # resultsフォルダ内のファイル名を出力する
 for file in results/*; do
   python3 remove_bugfile.py "$file"
-  python3 analyze_reconf_time.py "$file" >> reconf_time.csv
+  python3 analyze.py "$file" >> result.csv
 done
